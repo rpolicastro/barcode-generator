5b9236d4590d4fe2f82f47aac7cf6b50e903da5b
rpolicastro <31133814+rpolicastro@users.noreply.github.com>
2019-07-10 12:36:40 -0400

    rewriting parts of script

---
+++ config.R
@@ -0,0 +1,20 @@
+# base directory
+base.dir <- ""
+
+# number of barcodes required
+desired.barcodes <- 96
+
+# max iterations to try and achieve required barcode number
+iter.number <- 100
+
+# desired length of barcodes
+barcode.length <- 6
+
+# will ensure barcodes have at least this number of mismatches
+n.dist <- 2
+
+# maximum number of barcodes allowed to have a matching 3-mer in the same position
+max.nmer <- 6
+
+# maximum fraction of barcodes allowed to have the same base in the same position
+max.base.frac <- 0.35
